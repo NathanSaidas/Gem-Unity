e96b8357e1b7f2fe20bd70e6c091b6e460ecf93e
NathanSaidas <NathanSaidas@users.noreply.github.com>
2014-12-12 00:21:22 -0500

    Changes:
    • Added GameEvent system. See GameEvent and MonoGameEventHandler

---
+++ Unity Project/Project/Assets/Scripts/Gameplay/IGameEventReceiver.cs
@@ -0,0 +1,13 @@
+﻿#region CHANGE LOG
+/* December, 11, 2014 - Nathan Hanlan - Added interface IGameEventReceiver
+ * 
+ */
+#endregion
+
+namespace Gem
+{
+    public interface IGameEventReceiver
+    {
+        void OnReceiveEvent(GameEventData aData);
+    }
+}
