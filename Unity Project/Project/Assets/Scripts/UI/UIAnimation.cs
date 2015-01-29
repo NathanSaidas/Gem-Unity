fbeb14cb1c39e39af6712dec415900bc056a2221
NathanSaidas <NathanSaidas@users.noreply.github.com>
2015-01-29 00:26:08 -0500

    Changes:
    • Added UI Animations

---
+++ Unity Project/Project/Assets/Scripts/UI/UIAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Gem
+{
+    
+    public class UIAnimation : MonoBehaviour
+    {
+        [SerializeField]
+        private string m_IsOpenName = "IsOpen";
+        private Animator m_Animator = null;
+
+        private void Start()
+        {
+            m_Animator = GetComponent<Animator>();
+        }
+
+        public void Open()
+        {
+            m_Animator.SetBool(m_IsOpenName, true);
+        }
+        public void Close()
+        {
+            m_Animator.SetBool(m_IsOpenName, false);
+        }
+    }
+
+}
+
